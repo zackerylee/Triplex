dd403666f320d12637438a2734dc8315555848b1
Zack Douglas <zdouglas@amirsys.com>
2014-08-15 16:40:32 -0600

    add a new solid routine, clean up code

--- SequenceRoutines.h
+++ SequenceRoutines.h
@@ -1,37 +1,63 @@
 class AbstractSequence {
   public:
+    AbstractSequence(TriangleSet* triangleSet, int num) {
+      triangles = triangleSet;
+      triangleListSize = num;
+    };
     virtual void step() = 0;
+    inline void writeAll(int red, int green, int blue) {
+      for (int x = 0; x < triangleListSize; x++) {
+        triangles[x].manualWrite(red, blue, green);
+      }
+    }
+  protected:
+    TriangleSet* triangles;
+    int triangleListSize;
+
 };
 
-class NewSequence : public AbstractSequence {
+class SolidSequence : public AbstractSequence {
+    // super basic 3 color solid, probably thorw this away
   public:
-    inline NewSequence() {
-      i = 0;
+    inline SolidSequence(TriangleSet* triangleSet, int num) : AbstractSequence(triangleSet, num)  {
+      i = 1;
     };
     inline void step() {
-      i++;
+      switch (i % 3) {
+        case 0:
+          writeAll(0, 0, 255);
+          i = 0;
+          break;
+        case 1:
+          writeAll(255, 0, 0);
+          i++;
+          break;
+        case 2:
+          writeAll(0, 255, 0);
+          i++;
+          break;
+      }
     };
   private:
     int i;
 };
 
 class FadeSequence : public AbstractSequence {
+    // fade all levels between all colors at once
   public:
-    inline FadeSequence() {};
-    inline FadeSequence(TriangleSet* triangleSet, int num) {
+    inline FadeSequence(TriangleSet* triangleSet, int num) : AbstractSequence(triangleSet, num) {
       red = 255;
       blue = 1;
       green = 1;
       redInc = -1;
       greenInc = 1;
       blueInc = 0;
-      triangles = triangleSet;
-      triangleListSize = num;
     };
     inline void step() {
-      for (int x = 0; x < triangleListSize; x++) {
-        triangles[x].manualWrite(red, blue, green);
-      }
+      //      for (int x = 0; x < triangleListSize; x++) {
+      //        triangles[x].manualWrite(red, blue, green);
+      //      }
+      writeAll(red, blue, green);
       red += redInc;
       blue += blueInc;
       green += greenInc;
@@ -58,7 +84,5 @@ class FadeSequence : public AbstractSequence {
     int redInc;
     int blueInc;
     int greenInc;
-    TriangleSet* triangles;
-    int triangleListSize;
 };
 
--- Triplex.ino
+++ Triplex.ino
@@ -2,31 +2,53 @@
 #include "TriplexWriter.h"
 #include "SequenceRoutines.h"
 
-TriplexWriter triplexWriter = TriplexWriter();
+// Configuration Constants
 
 // set the number of levels on the triplex
 const int NUM_LEVELS = 2;
+// number of sequences
+const int NUM_SEQUENCES = 2;
+// amount of time for each sequence (5 minutes)
+const unsigned long TIME_SEQUENCE = (5 * 60 * 1000);
+
+// create the TriplexWriters
+TriplexWriter triplexWriter1 = TriplexWriter();
+//TriplexWriter triplexWriter2 = TriplexWriter(0x41);
+
+// Create the Triangle levels
 TriangleSet set1 = TriangleSet(3, 5, 6, triplexWriter);
 TriangleSet set2 = TriangleSet(9, 10, 11, triplexWriter);
 TriangleSet layers[] = {set1, set2};
-FadeSequence seq;
-NewSequence aseq;
+
+// Define the sequences
+FadeSequence seq = FadeSequence(layers, NUM_LEVELS);
+SolidSequence aseq = SolidSequence(layers, NUM_LEVELS);
 AbstractSequence *const sequences[2] = {&seq, &aseq};
-const int NUM_SEQUENCES = 2;
+
+// our state variables
+int currentSequence;
+unsigned long lastChange;
 
 void setup() {
-  seq = FadeSequence(layers, NUM_LEVELS);
-  aseq = NewSequence();
+  currentSequence = 0;
+  lastChange = 0;
 }
 
 void loop() {
+  if (millis() - lastChange > TIME_SEQUENCE) {
+    lastChange = millis();
+    
+    //TODO should we have something that fires between sequences? or just clear all the lights? 
+    
+    currentSequence += 1;
+    if (currentSequence > NUM_SEQUENCES) {
+      currentSequence = 0;
+    }
+    
+  }
   int delayVal = 5;
-  seq.step();
+  sequences[currentSequence]->step();
   delay(delayVal);
-  aseq.step();
-  
-  AbstractSequence *zzz = sequences[1];
-  zzz->step();
 }
 
 
