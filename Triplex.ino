79b9f6fef9126e26f34b7caca8d5a8b0be551728
Zack Douglas <zackerylee@gmail.com>
2015-08-27 00:46:37 -0600

    merging in Jake's changes

--- Triplex.ino
+++ Triplex.ino
@@ -10,24 +10,32 @@
 #define SHIELD_2 0x41
 
 // set the number of levels on the triplex
-const int NUM_LEVELS = 2;
+const int NUM_LEVELS = 9;
 // number of sequences
 const int NUM_SEQUENCES = 6; //3;
 // amount of time for each sequence (1 minutes)
-const unsigned long TIME_SEQUENCE = 60000;
+const unsigned long TIME_SEQUENCE = 30000;
 
 // create the TriplexWriters
 TriplexWriter triplexWriter1 = TriplexWriter(SHIELD_1);
-//TriplexWriter triplexWriter2 = TriplexWriter(SHIELD_2);
+TriplexWriter triplexWriter2 = TriplexWriter(SHIELD_2);
 
 // Create the Triangle levels that are on the first sheild
 // define: TriangleSet(red_pin, green_pin, blue_pin, shield)
-TriangleSet set1 = TriangleSet(3, 5, 6, triplexWriter1);
-TriangleSet set2 = TriangleSet(9, 10, 11, triplexWriter1);
+TriangleSet set1 = TriangleSet(0, 1, 2, triplexWriter1);
+TriangleSet set2 = TriangleSet(3, 4, 5, triplexWriter1);
+TriangleSet set3 = TriangleSet(6, 7, 8, triplexWriter1);
+TriangleSet set4 = TriangleSet(9, 10, 11, triplexWriter1);
+TriangleSet set5 = TriangleSet(12, 13, 14, triplexWriter1);
+
 
 // Triangles that are on the second shield
 //TriangleSet set3 = TriangleSet(3, 5, 6, triplexWriter2);
 //TriangleSet set4 = TriangleSet(9, 10, 11, triplexWriter2);
+TriangleSet set7 = TriangleSet(0, 1, 2, triplexWriter2);
+TriangleSet set8 = TriangleSet(3, 4, 5, triplexWriter2);
+TriangleSet set9 = TriangleSet(6, 7, 8, triplexWriter2);
+TriangleSet set6 = TriangleSet(9, 10, 11, triplexWriter2);
 
 TriangleSet layers[] = {
   set1, set2};
@@ -55,7 +63,7 @@ void setup() {
   currentSequence = 0;
   lastChange = 0;
   triplexWriter1.setup();
-  //triplexWriter2.setup();
+  triplexWriter2.setup();
 }
 
 void loop() {
@@ -73,7 +81,5 @@ void loop() {
   }
   sequences[currentSequence]->checkedStep();
 }
-
-
 
 
